1ce3a30f0cc8fe2abe258e8efe24cf60e37c57cf
mohammed-tahmid-sudo <mohammedtahmid.bd@gmail.com>
2025-08-04 21:54:20 +0600

    turns out that QT is a little bit harder than I thought but i'm not gonna back off

--- src/main.cpp
+++ src/main.cpp
@@ -1,52 +1,33 @@
 #include <QApplication>
-#include <QFrame>
 #include <QGridLayout>
 #include <QMainWindow>
+#include <QPushButton>
 #include <QWidget>
-#include <iostream>
-#include <vector>
 
-void print_tokens(const std::vector<std::vector<char>> &tokens) {
-  std::cout << "[";
-  bool first_line = true;
-  for (const auto &line : tokens) {
-    if (!first_line)
-      std::cout << ", ";
-    std::cout << "[";
-    bool first_token = true;
-    for (const auto &token : line) {
-      if (!first_token)
-        std::cout << ", ";
-      std::cout << "'" << token << "'";
-      first_token = false;
-    }
-    std::cout << "]";
-    first_line = false;
-  }
-  std::cout << "]";
-}
 int main(int argc, char *argv[]) {
+  int height = 600;
+  int weight = 600;
   QApplication app(argc, argv);
 
-  QWidget *central = new QWidget;
-  QGridLayout *layout = new QGridLayout(central);
+  QWidget *Centrlll = new QWidget;
+  QGridLayout *Layout = new QGridLayout(Centrlll);
+
+  QMainWindow window;
+  window.resize(height, weight);
 
   for (int row = 0; row < 8; row++) {
     for (int col = 0; col < 8; col++) {
-      QFrame *square = new QLable;
-      square->setFixedSize(50, 50);
-      square->setStyleSheet((row + col) % 2 == 0 ? "background: white;"
-                                                 : "background: gray;");
-      square->setAlignment(Qt::AlignCenter);
-      layout->addWidget(square, row, col);
+      QPushButton *btn = new QPushButton();
+      btn->setFixedSize(height / 8, weight / 8);
+      bool light = (row + col) % 2 == 0;
+      btn->setStyleSheet(light ? "background-color: #f0d9b5;"
+                               : // Light square color
+                             "background-color: #b58863;"); // Dark square
+      Layout->addWidget(btn, row, col);
     }
   }
-
-  QMainWindow window;
-  window.setWindowTitle("Default Window");
-  window.setCentralWidget(central);
-  window.resize(400, 300);
-
+  // Layout->addWidget(btn, row, col);
+  window.setCentralWidget(Centrlll);
   window.show();
   return app.exec();
 }
