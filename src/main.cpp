2147baa2697ef5b63c95533f9db19bce5bd39e3c
mohammed-tahmid-sudo <mohammedtahmid.bd@gmail.com>
2025-08-05 19:13:28 +0600

    a;slkdfj;alskjfalksjdf;laksjfd

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <QApplication>
+#include <map>
 #include <QGridLayout>
 #include <QMainWindow>
 #include <QPushButton>
@@ -26,6 +27,10 @@ int main(int argc, char *argv[]) {
       {"P", "P", "P", "P", "P", "P", "P", "P"}, // White pawns
       {"R", "N", "B", "Q", "K", "B", "N", "R"}  // White major pieces
   };
+  std::map<std::string, QString> pieceMap = {
+      {"p", "p.png"}, {"P", "P.png"}, {"k", "k.png"}, {"K", "K.png"},
+      {"r", "r.png"}, {"R", "R.png"}, {"n", "n.png"}, {"N", "N.png"},
+      {"b", "b.png"}, {"B", "B.png"}, {"q", "q.png"}, {"Q", "Q.png"}};
 
   for (int x = 0; x < 8; x++) {
     for (int y = 0; y < 8; y++) {
@@ -37,83 +42,9 @@ int main(int argc, char *argv[]) {
       else
         BTN->setStyleSheet("background-color: #779556");
 
-      if (Board[x][y] == "p") {
-        QPixmap Pixmap("/home/tahmid/programming_files/Chess-bluetooth/"
-                       "resources/Asset Images/p.png");
-        QIcon icon(Pixmap);
-        BTN->setIcon(icon);
-        BTN->setIconSize(Pixmap.size());
-      } else if (Board[x][y] == "P") {
-        QPixmap Pixmap("/home/tahmid/programming_files/Chess-bluetooth/"
-                       "resources/Asset Images/P.png");
-        QIcon icon(Pixmap);
-        BTN->setIcon(icon);
-        BTN->setIconSize(Pixmap.size());
-      } else if (Board[x][y] == "k") {
-        QPixmap Pixmap("/home/tahmid/programming_files/Chess-bluetooth/"
-                       "resources/Asset Images/k.png");
-        QIcon icon(Pixmap);
-        BTN->setIcon(icon);
-        BTN->setIconSize(Pixmap.size());
-      } else if (Board[x][y] == "K") {
-        QPixmap Pixmap("/home/tahmid/programming_files/Chess-bluetooth/"
-                       "resources/Asset Images/K.png");
-        QIcon icon(Pixmap);
-        BTN->setIcon(icon);
-        BTN->setIconSize(Pixmap.size());
-      } else if (Board[x][y] == "r") {
-        QPixmap Pixmap("/home/tahmid/programming_files/Chess-bluetooth/resources/Asset Images/r.png");
-        QIcon icon(Pixmap);
-        BTN->setIcon(icon);
-        BTN->setIconSize(Pixmap.size());
-      } else if (Board[x][y] == "R") {
-        QPixmap Pixmap("/home/tahmid/programming_files/Chess-bluetooth/resources/Asset Images/R.png");
-        QIcon icon(Pixmap);
-        BTN->setIcon(icon);
-        BTN->setIconSize(Pixmap.size());
-      } else if (Board[x][y] == "b") {
-        QPixmap Pixmap("/home/tahmid/programming_files/Chess-bluetooth/resources/Asset Images/b.png");
-        QIcon icon(Pixmap);
-        BTN->setIcon(icon);
-        BTN->setIconSize(Pixmap.size());
-      } else if (Board[x][y] == "B") {
-        QPixmap Pixmap("/home/tahmid/programming_files/Chess-bluetooth/resources/Asset Images/B.png");
-        QIcon icon(Pixmap);
-        BTN->setIcon(icon);
-        BTN->setIconSize(Pixmap.size());
-      } else if (Board[x][y] == "n") {
-        QPixmap Pixmap("/home/tahmid/programming_files/Chess-bluetooth/resources/Asset Images/n.png");
-        QIcon icon(Pixmap);
-        BTN->setIcon(icon);
-        BTN->setIconSize(Pixmap.size());
-      } else if (Board[x][y] == "N") {
-        QPixmap Pixmap("/home/tahmid/programming_files/Chess-bluetooth/resources/Asset Images/N.png");
-        QIcon icon(Pixmap);
-        BTN->setIcon(icon);
-        BTN->setIconSize(Pixmap.size());
-      } else if (Board[x][y] == "q") {
-        QPixmap Pixmap("/home/tahmid/programming_files/Chess-bluetooth/resources/Asset Images/q.png");
-        QIcon icon(Pixmap);
-        BTN->setIcon(icon);
-        BTN->setIconSize(Pixmap.size());
-      }else if (Board[x][y] == "Q") {
-        QPixmap Pixmap("/home/tahmid/programming_files/Chess-bluetooth/resources/Asset Images/Q.png");
-        QIcon icon(Pixmap);
-        BTN->setIcon(icon);
-        BTN->setIconSize(Pixmap.size());
-      } else if (Board[x][y] == "b") {
-        QPixmap Pixmap("/home/tahmid/programming_files/Chess-bluetooth/resources/Asset Images/b.png");
-        QIcon icon(Pixmap);
-        BTN->setIcon(icon);
-        BTN->setIconSize(Pixmap.size());
-      } else if (Board[x][y] == "b") {
-        QPixmap Pixmap("/home/tahmid/programming_files/Chess-bluetooth/resources/Asset Images/B.png");
-        QIcon icon(Pixmap);
-        BTN->setIcon(icon);
-        BTN->setIconSize(Pixmap.size());
-      } 
-
-      Layout->addWidget(BTN, x, y); } }
+       Layout->addWidget(BTN, x, y);
+    }
+  }
   // Layout->addWidget(btn, row, col);
   window.setCentralWidget(Centrlll);
   window.show();
